c98e3ccbb38b71f375cd5c8194bc4f32b52bcce7
realdegrees <>
2024-05-26 17:20:43 +0200

    added tldraw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+
+import { Tldraw } from "tldraw";
+
 export default function Home() {
   return (
     <main>
-      
+      <Tldraw className="fixed inset-0" hideUi/>
     </main>
   );
 }
