084fcb2644d398a02088817c76435a0dc04b83cc
realdegrees <>
2024-05-26 17:20:43 +0200

    added tldraw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+'use client';
+
+import { Tldraw } from "tldraw";
+
 export default function Home() {
   return (
-    <main>
-      
+    <main className="w-full h-full">
+      <Tldraw />
     </main>
   );
 }
