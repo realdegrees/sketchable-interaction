b904b7dc49285235983e5e9baead3fb300888d6b
realdegrees <>
2024-06-06 21:33:26 +0200

    added plugin getter for global store

--- src/stores/plugin.ts
+++ src/stores/plugin.ts
@@ -1,23 +1,26 @@
 import BasePlugin from "@/plugins/base";
+import { getDefineEnvPlugin } from "next/dist/build/webpack/plugins/define-env-plugin";
 import { create } from "zustand";
 export interface PluginData {
   selected?: BasePlugin;
   plugins: BasePlugin[];
+  getPlugin: (pluginId: string) => BasePlugin | undefined;
   setSelected: (pluginId: string) => void;
   register: (plugin: BasePlugin) => void;
 }
 
 // TODO adjust plugin store to not only store the id of the currently selected plugin but also a reference to all plugin instances
-export const usePluginStore = create<PluginData>((set) => ({
+export const usePluginStore = create<PluginData>((set, get) => ({
+  plugins: [],
+  getPlugin: (pluginId) => get().plugins.find(({ id }) => id === pluginId),
   setSelected: (pluginId) =>
     set((state) => ({
       ...state,
       selected: state.plugins.find(({ id }) => id === pluginId),
     })),
-  plugins: [],
   register: (plugin) =>
     set((state) => ({
       ...state,
-      plugins: [...state.plugins, plugin]
+      plugins: [...state.plugins, plugin],
     })),
 }));
