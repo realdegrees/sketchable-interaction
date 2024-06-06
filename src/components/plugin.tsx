32234baa8b59004378ea48a06a944790f645e788
realdegrees <>
2024-06-06 20:44:52 +0200

    moved plugin loading logic to client

--- src/components/plugin.tsx
+++ src/components/plugin.tsx
@@ -1,14 +1,32 @@
 'use client'
 
-import { PluginProps } from "@/plugins/base";
+import BasePlugin from "@/plugins/base";
 import { usePluginStore } from "@/stores/plugin";
+import { useEffect, useState } from "react";
 
-const Plugin = ({ props: { id, label } }: { props: PluginProps }) => {
-    const { setSelected } = usePluginStore();
+const Plugin = ({ name }: { name: string }) => {
+    const { setSelected, register } = usePluginStore();
+    const [plugin, setPlugin] = useState<BasePlugin>();
+
+    // Loads the plugin module's default export and registers it to the pluginStore and local state
+    useEffect(() => {
+        import(`../plugins/${name}/plugin`)
+            .then(({ default: plugin }) => {
+                register(plugin);
+                setPlugin(plugin);
+            });
+    }, [name, register, setPlugin])
+
+    if (!plugin) {
+        return <p>{`Loading\n${name}`}</p>
+    } else if (!(plugin instanceof BasePlugin)) {
+        return <p>{`Unable to load\n${name}`}</p>
+    };
+    const { id, label } = plugin.properties;
 
     return (
-        <div key={id} id={`plugin-${id}`} className="flex justify-center items-center w-14 h-14 rounded overflow-hidden hover:brightness-125" onClick={() => {
-            setSelected(`plugin-${id}`)
+        <div id={`plugin-${id}`} className="flex justify-center items-center w-14 h-14 rounded overflow-hidden hover:brightness-125" onClick={() => {
+            setSelected(id);
         }}>
             <p className="text-ellipsis absolute bottom-1 max-w-full overflow-hidden pointer-events-none">
                 {label ?? id}
--- src/components/pluginbar.tsx
+++ src/components/pluginbar.tsx
@@ -3,27 +3,23 @@ import { readdir } from "fs/promises";
 import path from "path";
 import Plugin from "./plugin";
 import CurrentPluginDisplay from "./currentPluginDisplay";
+import { readdirSync } from "fs";
 
 // Imports all
 const pluginDir = path.join(process.cwd(), 'src/plugins');
-const loadPlugins = readdir(pluginDir, { withFileTypes: true })
-    .then((dirent) => Promise.all<BasePlugin>(dirent
-        .filter(dirent => dirent.isDirectory())
-        .map(async dirent => (await import(`../plugins/${dirent.name}/plugin`)).default)));
+const pluginPaths = readdirSync(pluginDir, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
 
 // ! TODO: Dynamically adjust available tools when selecting a plugin (e.g. only rectangle for folder tool or only (customized-)draw tool for conveyor belt)
-
-const PluginBar = async () => {
-    const plugins = await loadPlugins;
-    
+const PluginBar = () => {
     // TODO get editor reference (might have to put PluginBar inside of tldraw context)
-
     return (
         <div id="pluginbar" className="absolute bottom-20 left-1/2 -translate-x-1/2 flex">
             <CurrentPluginDisplay />
             {
                 // ! Can pass funcion references here if needed as passing the entire instance is not supported in react
-                plugins.map(({ id, properties }) => <Plugin key={id} props={properties} />)
+                pluginPaths.map((name) => <Plugin key={name} name={name} />)
             }
         </div>
     )
