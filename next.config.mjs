dd46bdfa0546246dbd3566c5c4a154f714dfa817
realdegrees <>
2024-06-23 13:33:26 +0200

    added unplugin icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
+import Icons from 'unplugin-icons/webpack';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    webpack(config) {
+        config.plugins.push(
+            Icons({
+                compiler: 'jsx',
+                jsx: 'react',
+                autoInstall: true
+            })
+        );
+        return config;
+    }
 };
 
 export default nextConfig;
