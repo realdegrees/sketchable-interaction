00c4a4202dca432b21db627c873f912e1dcdc1b1
realdegrees <>
2024-06-23 12:02:16 +0200

    removed temporary current plugin display

--- src/components/pluginbar.tsx
+++ src/components/pluginbar.tsx
@@ -2,7 +2,6 @@ import BasePlugin from "@/plugins/base";
 import { readdir } from "fs/promises";
 import path from "path";
 import Plugin from "./plugin";
-import CurrentPluginDisplay from "./currentPluginDisplay";
 import { readdirSync } from "fs";
 
 // Imports all
@@ -11,13 +10,12 @@ const pluginPaths = readdirSync(pluginDir, { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name);
 
-// ! This could also be represented as a custom styles panelin tldraw https://github.com/tldraw/tldraw/blob/main/apps/examples/src/examples/shape-with-custom-styles/ShapeWithCustomStylesExample.tsx
+// ! This could also be represented as a custom styles panel in tldraw https://github.com/tldraw/tldraw/blob/main/apps/examples/src/examples/shape-with-custom-styles/ShapeWithCustomStylesExample.tsx
 // ! TODO: Dynamically adjust available tools when selecting a plugin (e.g. only rectangle for folder tool or only (customized-)draw tool for conveyor belt)
 const PluginBar = () => {
     // TODO get editor reference (might have to put PluginBar inside of tldraw context)
     return (
         <div className="flex flex-col absolute bottom-28 left-1/2 -translate-x-1/2 ">
-            <CurrentPluginDisplay />
             <div id="pluginbar" className="flex bg-zinc-900 rounded-lg border border-opacity-25 border-zinc-100">
                 {
                     // ! Can pass funcion references here if needed as passing the entire instance is not supported in react
