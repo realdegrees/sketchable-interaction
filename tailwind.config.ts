3373fe88c1d80d06770084a042214a73149b1a72
realdegrees <18383355+realdegrees@users.noreply.github.com>
2024-07-26 03:39:10 +0200

    fixes tailwind not picking up classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   mode: "jit",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-  safelist: [
-    {
-      pattern: /(bg|text|border)-(zinc|blue|red|yellow|green)-{400|500|600}/,
-      variants: ["!"],
-    },
-    { pattern: /opacity-[\d]{1,3}/, variants: ["!"] },
-    { pattern: /w-[\d]{1,3}/, variants: ["!"] },
-    { pattern: /h-[\d]{1,3}/, variants: ["!"] },
-    { pattern: /m(t|b|r|l)?-[\d]{1,3}/, variants: ["!", '-'] },
-    { pattern: /p(t|b|r|l)?-[\d]{1,3}/, variants: ["!", '-'] },
-    { pattern: /grid-.+/, variants: ["!"] },
-    { pattern: /rounded-.+/, variants: ["!"] },
-    { pattern: /text-(xl|lg)/, variants: ["!"] },
-    { pattern: /font-(bold)/, variants: ["!"] },
-    { pattern: /translate-(y|x)-(full|1\/2)/, variants: ["!", '-'] },
-    "grid",
-    "text-ellipsis"
-  ],
+  safelist: [  ],
 };
 export default config;
