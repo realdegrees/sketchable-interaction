edb1d8942b5a4310d7631a90597257097cf8af2d
realdegrees <18383355+realdegrees@users.noreply.github.com>
2024-08-21 23:58:48 +0200

    reworks toolbar loading logic to remove hardcoded stuff

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,25 +1,23 @@
 import { usePluginStore } from "@/stores/plugin";
-import { useTools, DefaultToolbar, TldrawUiMenuItem, DefaultToolbarContent, useIsToolSelected } from "tldraw"
-const toolList: string[] = ['rect', 'conveyor']
+import { useTools, DefaultToolbar, TldrawUiMenuItem, useIsToolSelected, TLUiToolsContextType } from "tldraw"
 const defaultTools: string[] = ['select', 'eraser']
-// ! This component decides which tools are shown in the toolbar for the currently selected plugin based on its properties
-const Toolbar = () => {
+
+const CustomTldrawUiMenuItem = ({ name }: { name: string}) => {
     const tools = useTools();
-    console.log(tools);
-    
+    const isSelected = useIsToolSelected(tools[name]);
+    return <TldrawUiMenuItem {...tools[name]} isSelected={isSelected} />;
+}
+
+
+// ! This component decides which tools are shown in the toolbar for the currently selected plugin based on its properties
+const Toolbar = () => {    
     const { plugins, selected } = usePluginStore();
     const selectedPluginAvailableShapes = plugins.find(({ plugin: { properties: { id } } }) => id === selected)?.plugin.properties.availableShapes;
-
-    // TODO look for a way to circumvent react's hook order rule in order to not have to hardcode this
-    const isRectSelected = useIsToolSelected(tools['rect']);
-    const isConveyorSelected = useIsToolSelected(tools['conveyor']);
 
     return (
         <div>
             <DefaultToolbar>
-                {defaultTools.map((tool) => <TldrawUiMenuItem key={`tool-${tool}`} {...tools[tool]} isSelected={useIsToolSelected.call(this, tools[tool])} />)}
-                {selectedPluginAvailableShapes?.includes('rect') ? <TldrawUiMenuItem {...tools['rect']} isSelected={isRectSelected} /> : undefined}
-                {selectedPluginAvailableShapes?.includes('conveyor') ? <TldrawUiMenuItem {...tools['conveyor']} isSelected={isConveyorSelected} /> : undefined}
+                {[...defaultTools, ...selectedPluginAvailableShapes ?? []].map((name) => <CustomTldrawUiMenuItem name={name} key={name} />)}
             </DefaultToolbar>
         </div>
     )
